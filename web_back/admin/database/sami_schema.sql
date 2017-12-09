5178d15354d101675072f68b9321222906d97a36
julitus <julioqq29@gmail.com>
2017-12-09 17:53:24 -0500

    fix database

--- web_back/admin/database/sami_schema.sql
+++ web_back/admin/database/sami_schema.sql
@@ -34,13 +34,48 @@ SET default_tablespace = '';
 SET default_with_oids = false;
 
 --
+-- Name: cops; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE cops (
+    id bigint NOT NULL,
+    dni character varying(8),
+    password character varying(128),
+    station_id bigint,
+    created timestamp without time zone,
+    modified timestamp without time zone
+);
+
+
+--
+-- Name: cops_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE cops_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: cops_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE cops_id_seq OWNED BY cops.id;
+
+
+--
 -- Name: stations; Type: TABLE; Schema: public; Owner: -
 --
 
 CREATE TABLE stations (
     id bigint NOT NULL,
-    dni character varying(8),
-    password character varying(128),
+    name character varying(128),
+    city character varying(128),
+    province character varying(128),
+    state character varying(128),
     created timestamp without time zone,
     modified timestamp without time zone
 );
@@ -101,6 +136,13 @@ ALTER SEQUENCE users_id_seq OWNED BY users.id;
 -- Name: id; Type: DEFAULT; Schema: public; Owner: -
 --
 
+ALTER TABLE ONLY cops ALTER COLUMN id SET DEFAULT nextval('cops_id_seq'::regclass);
+
+
+--
+-- Name: id; Type: DEFAULT; Schema: public; Owner: -
+--
+
 ALTER TABLE ONLY stations ALTER COLUMN id SET DEFAULT nextval('stations_id_seq'::regclass);
 
 
@@ -109,6 +151,14 @@ ALTER TABLE ONLY stations ALTER COLUMN id SET DEFAULT nextval('stations_id_seq':
 --
 
 ALTER TABLE ONLY users ALTER COLUMN id SET DEFAULT nextval('users_id_seq'::regclass);
+
+
+--
+-- Name: cops_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY cops
+    ADD CONSTRAINT cops_pkey PRIMARY KEY (id);
 
 
 --
--- web_back/admin/src/Model/MongoCollection/ReportsCollection.php
+++ web_back/admin/src/Model/MongoCollection/ReportsCollection.php
@@ -5,6 +5,23 @@ use CakeMonga\MongoCollection\BaseCollection;
 
 class ReportsCollection extends BaseCollection
 {
-	
+	public function insertTo($report)
+    {
+        return $this->insert(
+        	[
+	        	'dni' => $report['dni'], 
+	        	'location' => [
+	        		'type' => "Point",
+	        		'coordinates' => [
+	        			(float)$report['lat'], 
+	        			(float)$report['lng']
+	        		]
+	        	],
+	        	'intensity' => (int)$report['intesity'],
+	        	'modality' => (int),
+	        	'date' => date('c')
+	        ]
+        );
+    }
 
 }
