ca82a90ec722c18de534390e39a32effc5bbdbeb
julitus <julioqq29@gmail.com>
2017-12-09 16:32:53 -0500

    add stations table in database

--- web_back/admin/database/sami_schema.sql
+++ web_back/admin/database/sami_schema.sql
@@ -34,6 +34,38 @@ SET default_tablespace = '';
 SET default_with_oids = false;
 
 --
+-- Name: stations; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE stations (
+    id bigint NOT NULL,
+    dni character varying(8),
+    password character varying(128),
+    created timestamp without time zone,
+    modified timestamp without time zone
+);
+
+
+--
+-- Name: stations_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE stations_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: stations_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE stations_id_seq OWNED BY stations.id;
+
+
+--
 -- Name: users; Type: TABLE; Schema: public; Owner: -
 --
 
@@ -69,7 +101,22 @@ ALTER SEQUENCE users_id_seq OWNED BY users.id;
 -- Name: id; Type: DEFAULT; Schema: public; Owner: -
 --
 
+ALTER TABLE ONLY stations ALTER COLUMN id SET DEFAULT nextval('stations_id_seq'::regclass);
+
+
+--
+-- Name: id; Type: DEFAULT; Schema: public; Owner: -
+--
+
 ALTER TABLE ONLY users ALTER COLUMN id SET DEFAULT nextval('users_id_seq'::regclass);
+
+
+--
+-- Name: stations_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY stations
+    ADD CONSTRAINT stations_pkey PRIMARY KEY (id);
 
 
 --
