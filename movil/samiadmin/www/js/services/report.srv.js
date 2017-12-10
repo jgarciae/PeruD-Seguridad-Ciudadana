9ba49956a4c649bb4370595cf841c0f73ead2e2b
César Juárez <cesar.juarez@apselom.com>
2017-12-10 03:52:29 -0500

    find nearby points

---
+++ movil/samiadmin/www/js/services/report.srv.js
@@ -0,0 +1,28 @@
+angular.module('report.srv', [])
+
+    .factory('Report', function($http, $q, Config) {
+	return {
+            send: function(data) {
+		
+		var deferred = $q.defer();
+		var promise = deferred.promise;
+		
+		var link = Config.API_REPORT_SEND;
+
+		var config = {
+		    headers : {
+			'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'
+		    }
+		}
+
+		$http.post(link, data).then(function(response){
+		    deferred.resolve(response.data);
+		}, function(errResponse){
+		    deferred.reject(errResponse);
+		});
+
+		return promise;
+            }
+
+	}
+    })
