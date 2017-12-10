fe3bd4fb86405344aebcd862d4e804f9ed02b8ce
César Juárez <cesar.juarez@apselom.com>
2017-12-10 10:27:21 -0500

    view danger points, ready

--- movil/samiadmin/www/js/services/report.srv.js
+++ movil/samiadmin/www/js/services/report.srv.js
@@ -22,6 +22,28 @@ angular.module('report.srv', [])
 		});
 
 		return promise;
+            },
+
+	    getRatio: function(data) {
+		
+		var deferred = $q.defer();
+		var promise = deferred.promise;
+		
+		var link = Config.API_REPORT_GET_RATIO;
+
+		var config = {
+		    headers : {
+			'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'
+		    }
+		}
+		
+		$http.post(link, data).then(function(response){
+		    deferred.resolve(response.data);
+		}, function(errResponse){
+		    deferred.reject(errResponse);
+		});
+
+		return promise;
             }
 
 	}
